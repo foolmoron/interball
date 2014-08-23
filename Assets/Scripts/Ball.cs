1fa81f7c005f0e1bfe3a89fce5b43c669522a657
Momin Khan <foolmoron@gmail.com>
2014-08-23 00:35:25 -0400

    Basic main game that loads levels into a board

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    [Range(1, 20)]
+    public float Speed;
+    float currentSpeed;
+
+    Vector3 spawnPosition;
+
+    void Start() {
+        spawnPosition = transform.position;
+        Reset();
+        Move();
+    }
+
+    void Update() {
+        if (Speed != currentSpeed) {
+            SetSpeed(Speed);
+        }
+    }
+
+    public void Reset() {
+        transform.position = spawnPosition;
+        Stop();
+    }
+
+    public void Stop() {
+        rigidbody2D.velocity = Vector2.zero;
+        rigidbody2D.Sleep();
+    }
+
+    public void Move() {
+        SetSpeed(Speed);
+        SetDirectionVector(new Vector2(Random.value, Random.value));
+    }
+
+    public void SetSpeed(float speed) {
+        currentSpeed = speed;
+        rigidbody2D.velocity = rigidbody2D.velocity.normalized * currentSpeed;
+    }
+
+    public void SetDirectionRadians(float rad) {
+        SetDirectionVector(new Vector2(Mathf.Cos(rad), Mathf.Sin(rad)));
+    }
+
+    public void SetDirectionVector(Vector2 direction) {
+        rigidbody2D.velocity = direction.normalized * currentSpeed;
+    }
+}
---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+    public string[] LevelsToLoad;
+
+    Level[] levels;
+    List<Level> activeLevels;
+    List<Level> inactiveLevels;
+
+    void Start() {
+        StartCoroutine(LoadAllLevels(LevelsToLoad));
+    }
+
+    void InitializeBoard() {
+        if (levels.Length < 4)
+            Debug.LogError("Need at least 4 levels to make game work!");
+
+        inactiveLevels = new List<Level>(levels);
+        activeLevels = new List<Level>();
+        for (int i = 0; i < 4; i++) {
+            var randomIndex = Mathf.FloorToInt(Random.value * inactiveLevels.Count);
+            var level = inactiveLevels[randomIndex];
+            level.gameObject.SetActive(true);
+            level.LevelColor = new Color(Random.value, Random.value, Random.value);
+            level.transform.localRotation = Quaternion.Euler(0, 0, 90 * i);
+            activeLevels.Add(level);
+            inactiveLevels.RemoveAt(randomIndex);
+        }
+    }
+
+    IEnumerator LoadAllLevels(string[] sceneNames) {
+        for (int i = 0; i < sceneNames.Length; i++) {
+            var sceneName = sceneNames[i];
+            Application.LoadLevelAdditive(sceneName);
+        }
+        yield return new WaitForEndOfFrame(); // wait until next frame for levels to be initialized
+
+        levels = FindObjectsOfType<Level>();
+        for (int i = 0; i < levels.Length; i++) {
+            levels[i].transform.parent = transform;
+            levels[i].gameObject.SetActive(false);
+        }
+        InitializeBoard();
+    }
+	
+	void Update() {
+	}
+}
