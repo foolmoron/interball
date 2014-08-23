27743a8f0fcf3ba82e6e92dc75b8e1f882c425de
Momin Khan <foolmoron@gmail.com>
2014-08-22 23:25:41 -0400

    Project setup with test scenes

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Level : MonoBehaviour {
+
+    public float LevelAlpha = 0.25f;
+    public Color LevelColor;
+    Color oldLevelColor = Color.clear;
+
+	void Start() {
+	}
+	
+	void Update() {
+	    if (LevelColor != oldLevelColor) {
+	        var allSprites = GetComponentsInChildren<SpriteRenderer>();
+	        for (int i = 0; i < allSprites.Length; i++) {
+                allSprites[i].color = LevelColor.withAlpha(LevelAlpha);
+	        }
+
+	        oldLevelColor = LevelColor;
+	    }
+	}
+}
